d34c08b6c1b99deed4a2e3a90576ce24b2338438
Soulter <905617992@qq.com>
2024-03-27 18:57:48 +0800

    perf: UI perf

--- src/components/ChatAssistant.vue
+++ src/components/ChatAssistant.vue
@@ -1,41 +1,90 @@
 <template>
     <v-app>
-        <v-container fluid class="p-0" style="height: 100vh;">
-            <v-row class="m-0" style="height: 100%;">
-                <!--<v-col cols="12">
-                    <h2 class="title">#ChatAssistant</h2>
-                </v-col>-->
-                <v-col cols="12" class="chat-container">
-                    <div class="chat-messages" ref="chatMessages">
-                        <div v-for="(message, index) in messages" :key="index"
-                            :class="{ 'message-me': message.sender === 'me', 'message-other': message.sender !== 'me' }">
-                            <div class="message-content"
-                                :class="{ 'message-me-content': message.sender === 'me', 'message-other-content': message.sender !== 'me' }">
-                                {{ message.content }}</div>
-                        </div>
-                    </div>
-                    <div class="input-container">
-                        <v-btn @click="toggleInputMode" class="mode-switch-btn">
-                            <v-icon v-if="inputMode === 'text'">mdi-microphone</v-icon>
-                            <v-icon v-else>mdi-keyboard</v-icon>
-                        </v-btn>
-                        <v-text-field v-model="newMessage" placeholder="输入消息" outlined solo-inverted
-                            v-if="inputMode === 'text'" @input="onInput" @blur="onBlur"
-                            class="plain-text-field"></v-text-field>
-                        <v-btn v-if="inputMode === 'text'" @click="sendMessage" class="send-btn">
-                            <v-icon>mdi-airplane</v-icon>
-                        </v-btn>
-                        <v-btn v-else @touchstart="startRecording" @touchend="stopRecording" class="record-btn">
-                            <v-icon>mdi-microphone</v-icon> 按住说话
-                            <div class="recording-indicator" v-show="showRecordingIndicator">
-                                <div>{{ Math.floor(recordingTime / 60) }}:{{ (recordingTime % 60 < 10 ? '0' : '') +
-                            recordingTime % 60 }}</div>
-                                        <div style="margin-top: 10px;">正在录音</div>
-                                </div>
-                        </v-btn>
-                    </div>
-                </v-col>
-            </v-row>
+
+        <v-layout>
+            <v-app-bar style="background: linear-gradient(135deg, rgb(179, 208, 253) 0%, rgb(164, 202, 248) 100%);;"
+                color="" image="">
+
+                <template v-slot:prepend>
+                    <svg t="1695340928353" class="icon" viewBox="0 0 1024 1024" version="1.1"
+                        xmlns="http://www.w3.org/2000/svg" p-id="4185" width="40" height="40">
+                        <path
+                            d="M960 512c0 6.9-0.2 13.7-0.5 20.5-0.2 4.1-0.4 8.3-0.7 12.4-0.1 1.3-0.2 2.6-0.3 3.8-1.2 14.5-3 28.7-5.5 42.7 0 0.1-0.1 0.3-0.1 0.4-1.1 6.2-2.4 12.4-3.8 18.6-1.4 6.3-3 12.5-4.6 18.7 0 0.2-0.1 0.4-0.1 0.6-3.2 12-7 23.8-11.2 35.3-1.3 3.5-2.6 7-3.9 10.4-0.7 1.9-1.5 3.7-2.2 5.6-2 4.8-4 9.5-6.1 14.2-1.8 4-3.6 7.9-5.5 11.8-8.6 17.8-18.4 35-29.3 51.4-0.2 0.3-0.4 0.7-0.7 1-2.1 3.2-4.3 6.5-6.6 9.6-3.9 5.5-7.8 10.9-11.9 16.2-4.5 5.9-9.2 11.6-14 17.2-38 44.7-84.4 81.6-136.7 108.4C655 942.3 585.6 960 512 960c-118.7 0-226.5-46.1-306.7-121.4-4.4-4.2-8.8-8.4-13-12.8-12.5-12.7-24.3-26.2-35.2-40.4-3.3-4.3-6.6-8.7-9.7-13.1-2.9-4.1-5.8-8.2-8.6-12.4-0.2-0.2-0.3-0.5-0.5-0.7-0.1-0.2-0.3-0.4-0.4-0.6-2.8-4.2-5.5-8.4-8.1-12.7-7.7-12.5-14.8-25.4-21.2-38.7-1.5-3.1-3-6.2-4.4-9.4-1.8-3.8-3.4-7.7-5.1-11.6-1.2-2.8-2.3-5.5-3.4-8.3-0.5-1.1-0.9-2.3-1.4-3.5s-0.9-2.4-1.4-3.6c-0.7-1.9-1.5-3.9-2.2-5.8-2.5-7-4.9-14-7.1-21.2-1.4-4.7-2.8-9.4-4.1-14.1-0.1-0.2-0.1-0.5-0.2-0.7-3.5-12.8-6.4-25.9-8.7-39.2-2.4-13.5-4.2-27.3-5.3-41.2-0.1-1.2-0.2-2.5-0.3-3.8-0.2-2.1-0.3-4.2-0.4-6.4-0.1-2-0.2-4.1-0.3-6.1-0.3-6.8-0.5-13.7-0.5-20.5 0-8.2 0.2-16.2 0.7-24.3 0.2-4 0.5-8 0.8-12 0-0.3 0-0.7 0.1-1 0.9-10.6 2.1-21.1 3.7-31.5 0.7-4.3 1.4-8.5 2.2-12.7 1-5.6 2.2-11.2 3.4-16.7C119.6 213.6 298.3 64 512 64c63.6 0 124.1 13.2 178.9 37.2 26.9 11.7 52.4 26 76.2 42.5 90.8 63 157.1 158.7 182.1 270.1 1.2 5.5 2.4 11.1 3.4 16.7 0.8 4.2 1.5 8.4 2.1 12.6 1.7 10.8 3 21.6 3.8 32.7 0.8 10.1 1.3 20.2 1.4 30.5 0.1 1.8 0.1 3.8 0.1 5.7z"
+                            fill="#4D76FF" p-id="4186"></path>
+                        <path
+                            d="M680 605.7v125.6c0 19.7-16.1 35.7-35.7 35.7H379.8c-16.8 0-30.9-11.7-34.7-27.3-0.7-2.7-1-5.5-1-8.4V605.7c0-19.7 16.1-35.7 35.7-35.7h264.5c2.5 0 5 0.3 7.4 0.8 16.1 3.3 28.3 17.8 28.3 34.9z"
+                            fill="#A4E7FF" p-id="4187"></path>
+                        <path
+                            d="M680 605.7v125.6c0 19.7-16.1 35.7-35.7 35.7H379.8c-16.8 0-30.9-11.7-34.7-27.3h285.6c11.6 0 20.9-9.4 20.9-20.9v-148c16.2 3.3 28.4 17.8 28.4 34.9z"
+                            fill="#47D1FA" p-id="4188"></path>
+                        <path
+                            d="M830.3 493.8l-8.3 23-257.7 208c-15.2 12.3-33.8 18.2-52.3 17.6-16.6-0.5-33-6.2-46.8-17.1L201.8 516.8l-8.1-22.6c-19.8-55.1 3.3-116.5 54.5-144.8l17.6-9.8L269 324c11.4-56.6 61.7-96.8 119.4-95.6l42 0.9c45.9-42.9 117.2-42.9 163.1 0l42.6-0.5c58-0.7 108.2 40.4 118.8 97.4l2.3 12.5 18.9 10.6c33.7 18.8 55.2 52 60.1 88.2 0.3 2 0.5 3.9 0.7 5.9 1.5 16.6-0.6 33.8-6.6 50.4z"
+                            fill="#FFFFFF" p-id="4189"></path>
+                        <path
+                            d="M429.9 229.2l82 513.2-246.6-402.5 0.6-0.3L269 324c11.4-56.6 61.7-96.8 119.4-95.7l41.5 0.9zM758.7 339.5l-246.8 403 81.7-513.2 42.5-0.5c58-0.7 108.2 40.4 118.9 97.4l2.3 12.5 1.4 0.8z"
+                            fill="#A4E7FF" p-id="4190"></path>
+                        <path d="M385.6 732.5m-13.2 0a13.2 13.2 0 1 0 26.4 0 13.2 13.2 0 1 0-26.4 0Z" fill="#FFFFFF"
+                            p-id="4191"></path>
+                        <path d="M638.4 732.5m-13.2 0a13.2 13.2 0 1 0 26.4 0 13.2 13.2 0 1 0-26.4 0Z" fill="#FFFFFF"
+                            p-id="4192"></path>
+                        <path
+                            d="M830.3 493.8l-8.3 23-257.7 208c-15.2 12.3-33.8 18.2-52.3 17.6l324.9-299c1.5 16.6-0.6 33.8-6.6 50.4z"
+                            fill="#DDDDDD" p-id="4193"></path>
+                    </svg>
+                </template>
+
+                <v-app-bar-title
+                    style="font-family: 'ZCOOL KuaiLe', sans-serif; font-size: 26px;">还没想好</v-app-bar-title>
+
+
+                <v-btn style="font-weight: 1000;font-family: 'ZCOOL KuaiLe', sans-serif;" size="small"
+                    @click="jumpurl('https://www.ustb.edu.cn/')">
+                    <v-icon>mdi-home</v-icon>
+                    首页
+                </v-btn>
+
+            </v-app-bar>
+
+            <v-main>
+            </v-main>
+        </v-layout>
+
+        <v-container fluid class="p-0 chat-container" style="height: calc(100vh - 64px);">
+
+            <div class="chat-messages" ref="chatMessages" style="margin-bottom: 16px;">
+                <div v-for="(message, index) in messages" :key="index"
+                    :class="{ 'message-me': message.sender === 'me', 'message-other': message.sender !== 'me' }">
+                    <div class="message-content"
+                        :class="{ 'message-me-content': message.sender === 'me', 'message-other-content': message.sender !== 'me' }">
+                        {{ message.content }}</div>
+                </div>
+            </div>
+
+            <div class="recording-indicator" v-show="showRecordingIndicator">
+                <div>{{ Math.floor(recordingTime / 60) }}:{{ (recordingTime % 60 < 10 ? '0' : '') + recordingTime % 60 }}</div>
+                <div style="margin-top: 10px;">正在录音</div>
+            </div>
+
+            <div style="display: flex; align-items: center; justify-content: center;">
+                <div v-if="inputMode!=='text'" @touchstart="startRecording" @touchend="stopRecording" class="voice-card">
+                    <v-icon>mdi-microphone</v-icon>
+                    <span>按 住 说 话</span>
+                </div>
+
+                <div  v-if="inputMode==='text'" class="word-input" style="margin-bottom: 16px;">
+                    <input  type="text" class="search__input" placeholder="请输入" v-model="newMessage">
+                </div>
+
+                <div @click="sendMessage" class="send-btn"  v-if="inputMode==='text'" style="margin-bottom: 16px; margin-left: 16px">
+                    发送
+                </div>
+
+                <div @click="toggleInputMode" class="send-btn" style="margin-bottom: 16px; margin-left: 16px">
+                    <v-icon v-if="inputMode!=='text'">mdi-draw</v-icon>
+                    <v-icon v-if="inputMode==='text'">mdi-microphone</v-icon>
+                </div>
+            </div>
+
         </v-container>
     </v-app>
 </template>
@@ -49,10 +98,10 @@ export default {
     data() {
         return {
             messages: [
-                { sender: 'other', content: '你好，有什么需要帮助的吗？' }
+                { sender: 'other', content: '请问今天能帮你做什么呢？😊' }
             ],
             newMessage: '',
-            inputMode: 'text',
+            inputMode: 'voice',
         };
     },
     methods: {
@@ -61,30 +110,27 @@ export default {
         },
         async sendMessage() {
             if (this.newMessage.trim() !== '') {
-                try {
-                    this.messages.push({ sender: 'me', content: this.newMessage });
-                    const response = await axios.post('https://api.soulter.top/cccc2024/text',
-                        {
-                            "input_text": this.newMessage,
-                            "extra_prompt": "",
-                            "stream": true,
-                            "session_id": "123"
-                        }
-                    );
-                    const responseData = response.data;
-                    this.messages.push({ sender: 'other', content: responseData });
 
-                    window.Android.speakText(responseData);
-                } catch (error) {
-                    this.messages.push({ sender: 'other', content: error });
+                this.messages.push({ sender: 'me', content: this.newMessage });
+
+                axios.post('https://api.soulter.top/cccc2024/text', {
+                    "input_text": this.newMessage,
+                    "extra_prompt": "",
+                    "stream": true,
+                    "session_id": "123"
+                }).then(response => {
+                    console.log(response.data);
+                    this.messages.push({ sender: 'other', content: response.data });
+                }).catch(error => {
                     console.error(error);
-                }
+                });
+
                 this.newMessage = '';
-                //this.messages.push({ sender: 'other', content: '是的' });
                 this.scrollToBottom();
             }
         },
         async startRecording() {
+            console.log('start recording');
             try {
                 this.showRecordingIndicator = true;
                 this.recordingTime = 0; // 初始化录音时长
@@ -105,7 +151,7 @@ export default {
                 clearInterval(this.recordingInterval);
                 this.showRecordingIndicator = false;
                 window.Android.speechEnd();
-                while(!window.Android.confirmSpeechResult());
+                while (!window.Android.confirmSpeechResult());
                 var audio_b64 = window.Android.getSpeechResult();
                 //var audio_b64 = window.Android.fetchSpeechResultBase64();
                 this.newMessage = audio_b64;
@@ -139,18 +185,16 @@ export default {
 }
 
 .chat-container {
-    height: calc(100% - 60px);
     display: flex;
     flex-direction: column;
-    background: linear-gradient(to bottom, #f6d365, #fda085);
-    border-radius: 10px;
-    overflow: auto;
+    background: linear-gradient(135deg, rgb(231, 238, 249) 0%, rgb(212, 229, 250) 100%);
+    border-radius: 0px;
     box-shadow: 0 4px 6px rgba(0, 0, 0, 0.1);
 }
 
 .chat-messages {
     flex: 1;
-    overflow-y: auto;
+    overflow-y: scroll;
     padding: 10px;
     display: flex;
     flex-direction: column;
@@ -178,13 +222,23 @@ export default {
 }
 
 .send-btn {
-    max-width: 70px;
-    min-width: 70px;
-    border-radius: 0;
+    background-color: #ffffff;
+    color: #000;
+    border-radius: 9px;
+    padding: 15px 20px;
     font-weight: bold;
     font-size: 16px;
-    background-color: #6c5ce7;
-    color: #fff;
+    margin-right: 10px;
+    transition: 0.3s;
+    box-shadow: 0 10px 15px -3px rgba(0, 0, 0, 0.1), 0 4px 6px -2px rgba(0, 0, 0, 0.05);
+}
+
+.send-btn:hover {
+    background-color: #f0f0f0;
+}
+
+.send-btn:active {
+    background-color: #e0e0e0;
 }
 
 .record-btn {
@@ -207,23 +261,28 @@ export default {
 
 .message-me-content {
     display: flex;
-    background-color: #6c5ce7;
+    background-color: #79afe1;
     color: #fff;
     justify-content: flex-end;
     align-items: center;
-    border-radius: 5px;
+    border-radius: 8px;
     padding: 10px 20px;
     margin: 10px;
+    font-size: 20px;
 }
 
 .message-other-content {
     display: flex;
-    background-color: #f8f9fa;
+    width: 90%;
+    background-color: #5eadf7;
     justify-content: flex-start;
     align-items: center;
-    border-radius: 5px;
+    border-radius: 8px;
     padding: 10px 20px;
     margin: 10px;
+    color: #fff;
+    font-size: 20px;
+    letter-spacing: 1px;
 }
 
 .message-me {
@@ -265,4 +324,72 @@ export default {
     position: relative;
     touch-action: manipulation;
 }
+
+.voice-card {
+    background-color: #fff;
+    display: flex;
+    flex-direction: row;
+    align-items: center;
+    justify-content: center;
+    border-radius: 9px;
+    width: 70%;
+    gap: 16px;
+    margin-bottom: 16px;
+    padding: 16px;
+    box-shadow: 0 10px 15px -3px rgba(0, 0, 0, 0.1), 0 4px 6px -2px rgba(0, 0, 0, 0.05);
+    transition: 0.3s;
+    cursor: pointer;
+}
+
+.voice-card span {
+    font-weight: bold;
+    color: #333;
+}
+
+.voice-card:active {
+    transform: scale(0.95);
+    background-color: #f0f0f0;
+}
+
+.word-input {
+    width: 55%;
+}
+
+.card-title-warn {
+    padding: 1rem;
+    background-color: #fff;
+    box-shadow: 0 10px 15px -3px rgba(0, 0, 0, 0.1), 0 4px 6px -2px rgba(0, 0, 0, 0.05);
+    width: 90%;
+    border-radius: 20px;
+}
+
+
+.search__input {
+    font-family: "Open Sans", sans-serif;
+    font-size: 17px;
+    color: #333333;
+    background-color: #ffffff;
+    border: none;
+    padding: 15px 0px 15px 15px;
+    border-radius: 10px;
+    width: 100%;
+    transition: all 0.2s;
+    box-shadow: 0 10px 15px -3px rgba(0, 0, 0, 0.1), 0 4px 6px -2px rgba(0, 0, 0, 0.05);
+
+}
+
+.search__input:focus {
+    outline: none;
+    width: 100%;
+    background-color: #ffffff;
+}
+
+.search__input::-webkit-input-placeholder {
+    font-size: 12px;
+    text-transform: uppercase;
+    letter-spacing: 5px;
+    font-weight: 500;
+    color: #999999;
+}
+
 </style>
--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,94 +1,50 @@
 <template>
   <v-container>
-    <v-row class="text-center">
-      <v-col cols="12">
-        <v-img
-          :src="require('../assets/logo.svg')"
-          class="my-3"
-          contain
-          height="200"
-        />
-      </v-col>
+    <div class="card-title-warn">
+                        <div class="title" style="font-size: 13px;">
+                            <span>
+                                <svg t="1695436459025" class="icon" viewBox="0 0 1024 1024" version="1.1"
+                                    xmlns="http://www.w3.org/2000/svg" p-id="9276" width="20" height="20">
+                                    <path
+                                        d="M512 0c282.752 0 512 229.248 512 512S794.752 1024 512 1024 0 794.752 0 512 229.248 0 512 0z"
+                                        fill="#32ABD1" p-id="9277"></path>
+                                    <path
+                                        d="M276.544 672.6016a33.536 33.536 0 0 1-14.976-27.904v-257.024h-48.64c-12.928 0-23.424 10.496-23.424 23.424v304.64c0 12.928 10.496 23.424 23.424 23.424h163.968l-100.352-66.56z"
+                                        fill="#CCEBF4" p-id="9278"></path>
+                                    <path
+                                        d="M443.456 756.0576l-145.28-96.512c-9.344-6.144-14.976-16.64-14.976-27.904v-335.872a33.4848 33.4848 0 0 1 52.096-27.904l104.96 69.632a123.7248 123.7248 0 0 1 55.296 103.04v287.488c0 18.56-14.976 33.536-33.536 33.536-6.656 0.128-13.056-1.92-18.56-5.504"
+                                        fill="#FFFFFF" p-id="9279"></path>
+                                    <path
+                                        d="M747.584 672.6016a33.536 33.536 0 0 0 14.976-27.904v-257.024h48.64c12.928 0 23.424 10.496 23.424 23.424v304.64c0 12.928-10.496 23.424-23.424 23.424h-163.968l100.352-66.56z"
+                                        fill="#CCEBF4" p-id="9280"></path>
+                                    <path
+                                        d="M580.8 756.0576l145.28-96.512c9.344-6.144 14.976-16.64 14.976-27.904v-335.872a33.4848 33.4848 0 0 0-52.096-27.904l-104.96 69.632a123.7248 123.7248 0 0 0-55.296 103.04v287.488c0 18.56 14.976 33.536 33.536 33.536 6.528 0.128 13.056-1.92 18.56-5.504"
+                                        fill="#FFFFFF" p-id="9281"></path>
+                                </svg>
+                            </span>
+                            <p class="title-text">
+                                下一节课
+                            </p>
+                            <p class="percent">
+                                <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1792 1792" fill="currentColor"
+                                    height="20" width="20">
+                                    <path
+                                        d="M1408 1216q0 26-19 45t-45 19h-896q-26 0-45-19t-19-45 19-45l448-448q19-19 45-19t45 19l448 448q19 19 19 45z">
+                                    </path>
+                                </svg> 今天 15:20，逸夫楼507
+                            </p>
+                        </div>
+                        <div class="data">
+                            <p>
+                                计算机组成原理
+                            </p>
 
-      <v-col class="mb-4">
-        <h1 class="display-2 font-weight-bold mb-3">
-          Welcome to the Vuetify 3 Beta
-        </h1>
-
-
-        <p class="subheading font-weight-regular">
-          For help and collaboration with other Vuetify developers,
-          <br>please join our online
-          <a
-            href="https://community.vuetifyjs.com"
-            target="_blank"
-          >Discord Community</a>
-        </p>
-      </v-col>
-
-      <v-col
-        class="mb-5"
-        cols="12"
-      >
-        <h2 class="headline font-weight-bold mb-5">
-          What's next?
-        </h2>
-
-        <v-row justify="center">
-          <a
-            v-for="(next, i) in whatsNext"
-            :key="i"
-            :href="next.href"
-            class="subheading mx-3"
-            target="_blank"
-          >
-            {{ next.text }}
-          </a>
-        </v-row>
-      </v-col>
-
-      <v-col
-        class="mb-5"
-        cols="12"
-      >
-        <h2 class="headline font-weight-bold mb-5">
-          Important Links
-        </h2>
-
-        <v-row justify="center">
-          <a
-            v-for="(link, i) in importantLinks"
-            :key="i"
-            :href="link.href"
-            class="subheading mx-3"
-            target="_blank"
-          >
-            {{ link.text }}
-          </a>
-        </v-row>
-      </v-col>
-
-      <v-col
-        class="mb-5"
-        cols="12"
-      >
-        <h2 class="headline font-weight-bold mb-5">
-          Ecosystem
-        </h2>
-
-        <v-row justify="center">
-          <a
-            v-for="(eco, i) in ecosystem"
-            :key="i"
-            :href="eco.href"
-            class="subheading mx-3"
-            target="_blank"
-          >
-            {{ eco.text }}
-          </a>
-        </v-row>
-      </v-col>
-    </v-row>
+                            <div class="range">
+                                <div class="fill">
+                                </div>
+                            </div>
+                        </div>
+                    </div>
   </v-container>
 </template>
 
@@ -147,3 +103,86 @@ export default {
   }),
 }
 </script>
+
+<style>
+
+
+.card-title-warn {
+  padding: 1rem;
+  background-color: #fff;
+  box-shadow: 0 10px 15px -3px rgba(0, 0, 0, 0.1), 0 4px 6px -2px rgba(0, 0, 0, 0.05);
+  width: 90%;
+  border-radius: 20px;
+}
+
+.title {
+  display: flex;
+  align-items: center;
+}
+
+.title span {
+  position: relative;
+  padding: 0.5rem;
+  background-color: #10B981;
+  width: 1.5rem;
+  height: 1.5rem;
+  border-radius: 9999px;
+}
+
+.title span svg {
+  position: absolute;
+  top: 50%;
+  left: 50%;
+  transform: translate(-50%, -50%);
+  color: #ffffff;
+  height: 1rem;
+}
+
+.title-text {
+  margin-left: 0.5rem;
+  color: #374151;
+  font-size: 18px;
+}
+
+.percent {
+  margin-left: 0.5rem;
+  color: #02972f;
+  font-weight: 600;
+  display: flex;
+}
+
+
+.data {
+  display: flex;
+  flex-direction: column;
+  justify-content: flex-start;
+}
+
+.data p {
+  margin-top: 1rem;
+  margin-bottom: 1rem;
+  color: #1F2937;
+  font-size: 2rem;
+  line-height: 2.5rem;
+  font-weight: 700;
+  text-align: left;
+}
+
+.data .range {
+  position: relative;
+  background-color: #E5E7EB;
+  width: 100%;
+  height: 0.5rem;
+  border-radius: 0.25rem;
+}
+
+.data .range .fill {
+  position: absolute;
+  top: 0;
+  left: 0;
+  background-color: #10B981;
+  width: 76%;
+  height: 100%;
+  border-radius: 0.25rem;
+}
+</style>
